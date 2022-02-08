f2079e3dd98a7aa7b8f2dc9ebceca3d9c6097b32
jabadji <julien.abadji@inria.fr>
2022-02-08 15:30:29 +0100

    test: add test on whole compress op

--- src/compress.rs
+++ src/compress.rs
@@ -38,3 +38,72 @@ impl Runnable for CompressCorpus {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        fs::File,
+        io::{Read, Write},
+    };
+
+    use tempfile;
+
+    use crate::{impls::OscarDoc, ops::Compress};
+
+    pub fn setup_oscardoc() -> String {
+        let mut corpus = String::new();
+        for i in 0..10000 {
+            corpus.push_str(&format!(r#"{{"item":{}}}"#, i));
+            corpus.push('\n');
+        }
+
+        corpus
+    }
+
+    // the way of checking results is bad, since we merge then sort results
+    // we should rather check the individual files one by one
+    #[test]
+    fn test_compress() {
+        let content = setup_oscardoc();
+        let content: Vec<&str> = content.lines().collect();
+        let content_files = (&content).chunks(1000).into_iter();
+        let tmpdir = tempfile::tempdir().unwrap();
+        for (idx, chunk) in content_files.enumerate() {
+            // should be safe since it does not rely on rust destructor
+            // + it is in a tempfile that will be cleaned at the exit of the test
+            let tempfile_path = tmpdir.path().join(format!("file_{idx}.jsonl"));
+            let mut tempfile = File::create(tempfile_path).unwrap();
+            tempfile.write_all(chunk.join("\n").as_bytes()).unwrap();
+        }
+
+        // create destination path and compress
+        let tmpdst = tempfile::tempdir().unwrap();
+        OscarDoc::compress_folder(tmpdir.path(), tmpdst.path(), false, 1).unwrap();
+
+        println!(
+            "{:?}",
+            std::fs::read_dir(tmpdir.path())
+                .unwrap()
+                .collect::<Vec<_>>()
+        );
+        // let mut items_decompressed = Vec::new();
+
+        let mut decompressed_data = Vec::new();
+        for file in std::fs::read_dir(tmpdst.path()).unwrap() {
+            println!("file: {:?}", file);
+            // for file in split_files {
+            let file = file.unwrap();
+            let file = File::open(file.path()).unwrap();
+            let mut reader = flate2::read::GzDecoder::new(file);
+            let mut decompressed = String::new();
+            reader.read_to_string(&mut decompressed).unwrap();
+            decompressed_data.extend(decompressed.lines().map(|x| x.to_string()).into_iter());
+        }
+
+        // sort results
+        decompressed_data.sort();
+        let mut content = content;
+        content.sort();
+        assert_eq!(decompressed_data, content);
+    }
+}
