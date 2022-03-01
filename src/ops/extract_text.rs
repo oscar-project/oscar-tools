b77103a57420c6eda165ecfb5de85088499188b3
rmohamed <rmohamed@wifi-pro-82-178.paris.inria.fr>
2022-03-01 15:09:32 +0100

    feat, extract the content of a json file

--- src/ops/extract_text.rs
+++ src/ops/extract_text.rs
@@ -5,5 +5,39 @@ use std::path::Path;
 
 use crate::error::Error;
 pub trait ExtractText {
-    fn extract_text(src: &Path, dst: &Path, del_src: bool) -> Result<(), Error>;
+    fn extract_text(src: &Path, dst: &Path, del_src: bool) -> Result<(), Error> {
+        todo!()
+    }
+    fn extact_json(doc: &str) -> Result<String, Error> {
+        let document: serde_json::Value = serde_json::from_str(doc)?;
+        match &document["content"] {
+            serde_json::Value::String(content) => Ok(content.to_string()),
+            other => Err(Error::MalformedContent(other.clone())),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::error::Error;
+
+    use super::ExtractText;
+    struct TestExtract;
+    impl ExtractText for TestExtract {}
+    #[test]
+    fn test_extract_json() {
+        let test = r#"{"content":"foo"}"#;
+        let res = TestExtract::extact_json(test);
+        assert_eq!("foo", res.unwrap());
+    }
+    #[test]
+    fn test_not_string() {
+        let test = r#"{"content":22}"#;
+        let res = TestExtract::extact_json(test);
+        assert!(res.is_err());
+        match res.unwrap_err() {
+            Error::MalformedContent(_) => assert!(true),
+            _ => assert!(false),
+        }
+    }
 }
